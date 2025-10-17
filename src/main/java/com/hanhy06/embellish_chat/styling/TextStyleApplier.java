aa51ecaadad75f68879398cb0e745b440e3fac78
조환희 <whghksgmls@gmail.com>
2025-10-17 14:50:55 +0900

    Refactor text styling: Simplify TextStyleApplier by delegating style functions to TextStyleFunctions

--- src/main/java/com/hanhy06/embellish_chat/styling/TextStyleApplier.java
+++ src/main/java/com/hanhy06/embellish_chat/styling/TextStyleApplier.java
@@ -1,101 +1,30 @@
 package com.hanhy06.embellish_chat.styling;
 
-import com.hanhy06.embellish_chat.EmbellishChat;
-import com.hanhy06.embellish_chat.config.ConfigManager;
-import com.hanhy06.embellish_chat.data.Config;
-import com.hanhy06.embellish_chat.styling.utile.Runs;
-import net.minecraft.text.*;
-import net.minecraft.util.Identifier;
+import net.minecraft.text.MutableText;
 
-import java.awt.*;
-import java.net.URI;
-
-import static com.hanhy06.embellish_chat.styling.utile.TextStyleUtils.flatten;
-import static com.hanhy06.embellish_chat.styling.utile.TextStyleUtils.slice;
+import java.util.function.BiFunction;
 
 public enum TextStyleApplier {
-    COLOR_HEX{
-        public MutableText apply(MutableText text,String option){
-            int color = Color.decode(option).getRGB();
-            return text.fillStyle(Style.EMPTY.withColor(color));
-        }
-    },
-    COLOR_PRESET{
-        public MutableText apply(MutableText text,String option){
-            Config config = ConfigManager.getConfig();
-            int color = config.colorPreset().getOrDefault(option,config.chatColor());
-            return text.fillStyle(Style.EMPTY.withColor(color));
-        }
-    },
-    COLOR_RAINBOW{
-        public MutableText apply(MutableText text,String option){
-            Runs runs = flatten(text);
-            String string = runs.full();
-            int length = string.length();
-            if (length == 0) return text;
+    COLOR_HEX(TextStyleFunctions::COLOR_HEX),
+    COLOR_PRESET(TextStyleFunctions::COLOR_PRESET),
+    COLOR_RAINBOW(TextStyleFunctions::COLOR_RAINBOW),
+    COLOR_GRADIENT((text,option) -> text),
+    COLOR_SHADOW((text,option) -> text),
+    FONT(TextStyleFunctions::FONT),
+    URL(TextStyleFunctions::URL),
+    BOLD(TextStyleFunctions::BOLD),
+    ITALIC(TextStyleFunctions::ITALIC),
+    UNDERLINE(TextStyleFunctions::UNDERLINE),
+    STRIKETHROUGH(TextStyleFunctions::STRIKETHROUGH),
+    OBFUSCATED(TextStyleFunctions::OBFUSCATED);
 
-            MutableText out = Text.empty();
-            for (int i = 0; i < length; i++) {
-                float hue = (float) i / length;
-                int rgb = Color.HSBtoRGB(hue, 0.7f, 1f);
-                out.append(slice(runs, i, i + 1).fillStyle(Style.EMPTY.withColor(rgb)));
-            }
-            return out;
-        }
-    },
-    COLOR_GRADIENT{
-        public MutableText apply(MutableText text,String option){
-            return Text.empty();
-        }
-    },
-    COLOR_SHADOW{
-        public MutableText apply(MutableText text,String option){
-            return Text.empty();
-        }
-    },
-    FONT{
-        public MutableText apply(MutableText text,String option){
-            StyleSpriteSource font = new StyleSpriteSource.Font(Identifier.tryParse(option));
-            return text.fillStyle(Style.EMPTY.withFont(font));
-        }
-    },
-    URL{
-        public MutableText apply(MutableText text,String option){
-            try {
-                URI uri = URI.create(option);
-                ClickEvent clickEvent = new ClickEvent.OpenUrl(uri);
-                return text.fillStyle(Style.EMPTY.withClickEvent(clickEvent).withColor(ConfigManager.getConfig().urlColor()));
-            } catch (IllegalArgumentException e) {
-                EmbellishChat.LOGGER.warn("Invalid URL address: {}", option);
-                return text;
-            }
-        }
-    },
-    BOLD{
-        public MutableText apply(MutableText text,String option){
-            return text.fillStyle(Style.EMPTY.withBold(true));
-        }
-    },
-    ITALIC{
-        public MutableText apply(MutableText text,String option){
-            return text.fillStyle(Style.EMPTY.withItalic(true));
-        }
-    },
-    UNDERLINE{
-        public MutableText apply(MutableText text,String option){
-            return text.fillStyle(Style.EMPTY.withUnderline(true));
-        }
-    },
-    STRIKETHROUGH{
-        public MutableText apply(MutableText text,String option){
-            return text.fillStyle(Style.EMPTY.withStrikethrough(true));
-        }
-    },
-    OBFUSCATED{
-        public MutableText apply(MutableText text,String option){
-            return text.fillStyle(Style.EMPTY.withObfuscated(true));
-        }
-    };
+    private final BiFunction<MutableText, String, MutableText> applierFunction;
 
-    public abstract MutableText apply(MutableText text, String option);
+    TextStyleApplier(BiFunction<MutableText, String, MutableText> applierFunction) {
+        this.applierFunction = applierFunction;
+    }
+
+    public MutableText apply(MutableText text, String option) {
+        return this.applierFunction.apply(text, option);
+    }
 }
---
+++ src/main/java/com/hanhy06/embellish_chat/styling/TextStyleFunctions.java
@@ -0,0 +1,78 @@
+package com.hanhy06.embellish_chat.styling;
+
+import com.hanhy06.embellish_chat.EmbellishChat;
+import com.hanhy06.embellish_chat.config.ConfigManager;
+import com.hanhy06.embellish_chat.data.Config;
+import com.hanhy06.embellish_chat.styling.utile.Runs;
+import net.minecraft.text.*;
+import net.minecraft.util.Identifier;
+
+import java.awt.*;
+import java.net.URI;
+
+import static com.hanhy06.embellish_chat.styling.utile.TextStyleUtils.flatten;
+import static com.hanhy06.embellish_chat.styling.utile.TextStyleUtils.slice;
+
+public class TextStyleFunctions {
+    public static MutableText COLOR_HEX(MutableText text, String option){
+        int color = Color.decode(option).getRGB();
+        return text.fillStyle(Style.EMPTY.withColor(color));
+    }
+
+    public static MutableText COLOR_PRESET(MutableText text, String option){
+        Config config = ConfigManager.getConfig();
+        int color = config.colorPreset().getOrDefault(option,config.chatColor());
+        return text.fillStyle(Style.EMPTY.withColor(color));
+    }
+
+    public static MutableText COLOR_RAINBOW(MutableText text, String option){
+        Runs runs = flatten(text);
+        String string = runs.full();
+        int length = string.length();
+        if (length == 0) return text;
+
+        MutableText out = Text.empty();
+        for (int i = 0; i < length; i++) {
+            float hue = (float) i / length;
+            int rgb = Color.HSBtoRGB(hue, 0.7f, 1f);
+            out.append(slice(runs, i, i + 1).fillStyle(Style.EMPTY.withColor(rgb)));
+        }
+        return out;
+    }
+
+    public static MutableText FONT(MutableText text, String option){
+        StyleSpriteSource font = new StyleSpriteSource.Font(Identifier.tryParse(option));
+        return text.fillStyle(Style.EMPTY.withFont(font));
+    }
+
+    public static MutableText URL(MutableText text, String option){
+        try {
+            URI uri = URI.create(option);
+            ClickEvent clickEvent = new ClickEvent.OpenUrl(uri);
+            return text.fillStyle(Style.EMPTY.withClickEvent(clickEvent).withColor(ConfigManager.getConfig().urlColor()));
+        } catch (IllegalArgumentException e) {
+            EmbellishChat.LOGGER.warn("Invalid URL address: {}", option);
+            return text;
+        }
+    }
+
+    public static MutableText BOLD(MutableText text, String option){
+        return text.fillStyle(Style.EMPTY.withBold(true));
+    }
+
+    public static MutableText ITALIC(MutableText text, String option){
+        return text.fillStyle(Style.EMPTY.withItalic(true));
+    }
+
+    public static MutableText UNDERLINE(MutableText text, String option){
+        return text.fillStyle(Style.EMPTY.withUnderline(true));
+    }
+
+    public static MutableText STRIKETHROUGH(MutableText text, String option){
+        return text.fillStyle(Style.EMPTY.withStrikethrough(true));
+    }
+
+    public static MutableText OBFUSCATED(MutableText text, String option){
+        return text.fillStyle(Style.EMPTY.withObfuscated(true));
+    }
+}
