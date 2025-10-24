d24f80f5f1652eedaa7d17e25668115049f51065
조환희 <whghksgmls@gmail.com>
2025-10-24 23:51:25 +0900

    Refactor Config: Add configVersion field to enhance configuration management

--- src/main/java/com/hanhy06/embellish_chat/config/Config.java
+++ src/main/java/com/hanhy06/embellish_chat/config/Config.java
@@ -8,6 +8,8 @@ import java.util.*;
 import static java.util.Map.entry;
 
 public record Config(
+        int configVersion,
+
         //style
         HashMap<String,List<StylingRule>> stylingRules,
 
@@ -33,6 +35,7 @@ public record Config(
 {
     public static Config createDefault(){
         return new Config(
+                1,
                 //style
                 //TODO: 나중에 LuckPerms쓸때 각 그룹이 어떤 스타일링을 할지 그룹 이름이 키로 써야함
                 new HashMap<>(Map.ofEntries(
