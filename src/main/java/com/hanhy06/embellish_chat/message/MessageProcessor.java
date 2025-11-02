e9c4764688c6d9794beb50b665a2d9c3aad9feaf
조환희 <whghksgmls@gmail.com>
2025-11-02 19:51:29 +0900

    Refactor MessageProcessor: Improve code readability by formatting and organizing method logic and control structures

--- src/main/java/com/hanhy06/embellish_chat/message/MessageProcessor.java
+++ src/main/java/com/hanhy06/embellish_chat/message/MessageProcessor.java
@@ -29,7 +29,7 @@ public class MessageProcessor implements ConfigListener {
     private Config config;
     private List<UUID> bannedPlayerList;
 
-    public MessageProcessor(MentionProcessor mentionProcessor, StylingProcessor stylingManager,PlayerManager playerManager) {
+    public MessageProcessor(MentionProcessor mentionProcessor, StylingProcessor stylingManager, PlayerManager playerManager) {
         INSTANCE = this;
         this.mentionProcessor = mentionProcessor;
         this.stylingManager = stylingManager;
@@ -43,42 +43,49 @@ public class MessageProcessor implements ConfigListener {
     }
 
     public SignedMessage handleMessage(SignedMessage message) {
-        if (bannedPlayerList.contains(message.getSender())) return message;
+        if (bannedPlayerList.contains(message.getSender())) {
+            return message;
+        }
 
         ServerPlayerEntity sender = playerManager.getPlayer(message.getSender());
         MutableText textMessage = message.getContent().copy();
         String stringMessage = message.getContent().getString();
 
-        List<Mention> mentions = handleMention(sender,stringMessage);
-        textMessage = stylingManager.applyMention(textMessage,mentions);
+        List<Mention> mentions = handleMention(sender, stringMessage);
+        textMessage = stylingManager.applyMention(textMessage, mentions);
 
-        for (String key: getPermissions(sender,"chat",config.stylingRules().keySet())){
-            textMessage = stylingManager.applyStylingRule(textMessage,key);
+        for (String key : getPermissions(sender, "chat", config.stylingRules().keySet())) {
+            textMessage = stylingManager.applyStylingRule(textMessage, key);
         }
 
         return message.withUnsignedContent(textMessage);
     }
 
-    private List<Mention> handleMention(ServerPlayerEntity sender,String message){
+    private List<Mention> handleMention(ServerPlayerEntity sender, String message) {
         List<Mention> mentions = new ArrayList<>();
-        if (sender ==null || message.isBlank()) return mentions;
-
-        List<String> keys = getPermissions(sender,"mention",config.mentionRules().keySet());
-        Set<ParsedMention> parsedMentions = new HashSet<>();
-        for (String key : keys){
-            parsedMentions.addAll(mentionProcessor.parseMentions(message,key));
+        if (sender == null || message.isBlank()) {
+            return mentions;
         }
-        List<ParsedTarget> parsedTargets = mentionProcessor.parseTargets(sender,parsedMentions);
+
+        List<String> keys = getPermissions(sender, "mention", config.mentionRules().keySet());
+        Set<ParsedMention> parsedMentions = new HashSet<>();
+        for (String key : keys) {
+            parsedMentions.addAll(mentionProcessor.parseMentions(message, key));
+        }
+        List<ParsedTarget> parsedTargets = mentionProcessor.parseTargets(sender, parsedMentions);
 
         Set<ServerPlayerEntity> targets = new HashSet<>();
         parsedTargets.forEach(target -> targets.addAll(target.players()));
-        if (!targets.isEmpty() && FabricLoader.getInstance().isModLoaded("luckperms")){
-            mentionProcessor.broadcastMentions(
-                    sender,
-                    targets.stream().filter(LuckPermsUtil::getNotification).collect(Collectors.toSet())
-            );
-        } else if (!targets.isEmpty()) {
-            mentionProcessor.broadcastMentions(sender, targets);
+
+        if (!targets.isEmpty()) {
+            if (FabricLoader.getInstance().isModLoaded("luckperms")) {
+                mentionProcessor.broadcastMentions(
+                        sender,
+                        targets.stream().filter(LuckPermsUtil::getNotification).collect(Collectors.toSet())
+                );
+            } else {
+                mentionProcessor.broadcastMentions(sender, targets);
+            }
         }
 
         parsedTargets.forEach(target -> mentions.add(target.createMention()));
