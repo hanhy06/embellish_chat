17c8983baf8094477c9ad47ff6e7e4ceaba7b4cd
조환희 <whghksgmls@gmail.com>
2025-11-14 20:48:46 +0900

    Increase maximum count limit from 500 to 1000 in EmbellishChatCommand; update feedback message accordingly

--- src/main/java/io/github/hanhy06/embellishchat/command/EmbellishChatCommand.java
+++ src/main/java/io/github/hanhy06/embellishchat/command/EmbellishChatCommand.java
@@ -138,7 +138,7 @@ public class EmbellishChatCommand {
             return 0;
         }
 
-        if (count > 500) {
+        if (count > 1000) {
             source.sendFeedback(() -> Text.literal("Count is too large. (Max 500)"), true);
             return 0;
         }
