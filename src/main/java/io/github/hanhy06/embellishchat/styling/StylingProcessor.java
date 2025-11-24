9b6ac68a4d5599683f9cc4f947af02d79f422a89
조환희 <whghksgmls@gmail.com>
2025-11-24 16:18:21 +0900

    feat(StylingProcessor): add applyStyleAPI method for enhanced styling application

--- src/main/java/io/github/hanhy06/embellishchat/styling/StylingProcessor.java
+++ src/main/java/io/github/hanhy06/embellishchat/styling/StylingProcessor.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static io.github.hanhy06.embellishchat.styling.util.TextSliceUtil.flatten;
 import static io.github.hanhy06.embellishchat.styling.util.TextSliceUtil.slice;
@@ -50,6 +51,42 @@ public class StylingProcessor implements ConfigListener {
         for (StylingRule rule : rules) {
             result = applyStyles(result,rule,player);
         }
+
+        return result;
+    }
+
+
+    /**
+     * @param text This is the full text to apply the style to.
+     * @param pattern This is the compiled regular expression for finding the parts where styling should be applied.
+     * @param functions These are the functions that apply styling.
+     * @param presets These are the presets to be used in the functions. Their length must match that of the functions.
+     * @param player This is the ServerPlayerEntity required for the Placeholder API.
+     * @return This is the text with styling applied.
+     */
+    public MutableText applyStyleAPI(MutableText text, Pattern pattern,List<Function<StyleParameter,MutableText>> functions,List<String> presets,ServerPlayerEntity player){
+        Matcher matcher = pattern.matcher(text.getString());
+        if (!matcher.find()) return text;
+
+        Runs runs = flatten(text);
+        MutableText result = Text.empty();
+        int lastEnd = 0;
+
+        do {
+            result.append(slice(runs,lastEnd,matcher.start()));
+
+            MutableText segment = slice(runs, matcher.start(1), matcher.end(1));
+            List<String> options = OptionUtil.split(matcher.group(2),config.delimiter());
+            options = OptionUtil.parseOption(options,presets,player);
+            for (int i=0;i<functions.size();i++){
+                StyleParameter parameter = StyleParameter.of(segment,options.get(i),player);
+                segment = functions.get(i).apply(parameter);
+            }
+
+            result.append(segment);
+            lastEnd = matcher.end();
+        }while (matcher.find());
+        result.append(slice(runs, lastEnd, runs.full().length()));
 
         return result;
     }
