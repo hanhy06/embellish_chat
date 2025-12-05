9eba8506d554fd0e8c009f888241fb36c3fdcaa2
조환희 <whghksgmls@gmail.com>
2025-12-05 19:49:00 +0900

    docs: add TODO comment for implementing %end% token handling in SyntaxStructure.java

--- src/main/java/io/github/hanhy06/embellishchat/parser/SyntaxStructure.java
+++ src/main/java/io/github/hanhy06/embellishchat/parser/SyntaxStructure.java
@@ -4,6 +4,7 @@ public record SyntaxStructure(
 //      TODO:토큰,스택 구조의 커스텀 파서 만들어야 함
 //      TODO:동작 은 eval 같은거 쓰면 위험하니 현제와 같은 구조 쓰거나 적당히 바꿔서 씀
 //      TODO:구조는 대략적으로 일케 중간고사 끝나고 알아서 구현하샘
+//      TODO:스타트 앤드 같은건 맨션같은거 위해 %end%같은거 알아서 구현 하슈
         ContentVerifyType tokenType,
         String tokenStart,
         String tokenEnd,
