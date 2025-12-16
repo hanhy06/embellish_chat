71f1a25684ccce6808a3a64c938d8e86aa1371d8
조환희 <whghksgmls@gmail.com>
2025-12-16 18:37:14 +0900

    fix: add missing line break in DiscordMessenger class for code clarity

--- src/main/java/io/github/hanhy06/embellishchat/discord/DiscordMessenger.java
+++ src/main/java/io/github/hanhy06/embellishchat/discord/DiscordMessenger.java
@@ -11,6 +11,7 @@ import java.net.http.HttpResponse;
 
 public class DiscordMessenger implements ConfigListener {
     public static DiscordMessenger INSTANCE;
+
     private final HttpClient client;
     private final Gson gson = new Gson();
 
