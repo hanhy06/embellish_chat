4bfd7c9bcdd00bfb55db7e639f6deed365f41f14
조환희 <whghksgmls@gmail.com>
2026-01-17 23:07:17 +0900

    fix: refactor mention and styling processing to use action-based handling

--- src/main/java/io/github/hanhy06/embellishchat/mention/rule/MentionParameter.java
+++ src/main/java/io/github/hanhy06/embellishchat/mention/rule/MentionParameter.java
@@ -1,10 +1,9 @@
 package io.github.hanhy06.embellishchat.mention.rule;
 
-import io.github.hanhy06.embellishchat.util.PlaceHolderUtil;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public record MentionParameter(ServerPlayerEntity sender, String option) {
     public static MentionParameter of(ServerPlayerEntity sender,String option){
-        return new MentionParameter(sender, PlaceHolderUtil.parsedText(option,sender).getString());
+        return new MentionParameter(sender, option);
     }
 }
