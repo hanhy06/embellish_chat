bdf65623e23885019cf27799004e500292a1d46c
조환희 <whghksgmls@gmail.com>
2026-01-26 14:33:17 +0900

    feat: update log message format in onInitialize method for improved clarity

--- src/main/java/io/github/hanhy06/embellishchat/EmbellishChat.java
+++ src/main/java/io/github/hanhy06/embellishchat/EmbellishChat.java
@@ -26,7 +26,7 @@ public class EmbellishChat implements ModInitializer {
 
     @Override
 	public void onInitialize() {
-        LOGGER.info("Initializing {}...", MOD_ID);
+        LOGGER.info("[{}] Initializing.", MOD_ID);
 
 		ServerLifecycleEvents.SERVER_STARTED.register(EmbellishChat::handleServerStart);
 
