c7f6feaec1b8ac4b11d9ead1677060a2cdfb33d0
조환희 <whghksgmls@gmail.com>
2026-01-27 19:10:04 +0900

    feat: enhance slot click handling for written book interaction in InventoryScreenHandler

--- src/main/java/io/github/hanhy06/embellishchat/inventory/InventoryScreenHandler.java
+++ src/main/java/io/github/hanhy06/embellishchat/inventory/InventoryScreenHandler.java
@@ -3,10 +3,16 @@ package io.github.hanhy06.embellishchat.inventory;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.network.packet.s2c.play.OpenWrittenBookS2CPacket;
+import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Hand;
 
 public class InventoryScreenHandler extends GenericContainerScreenHandler {
     public InventoryScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory) {
@@ -15,9 +21,25 @@ public class InventoryScreenHandler extends GenericContainerScreenHandler {
 
     @Override
     public void onSlotClick(int slotIndex, int button, SlotActionType actionType, PlayerEntity player) {
-        if ((slotIndex >= 0 && slotIndex < 54) || actionType.equals(SlotActionType.PICKUP_ALL)) {
+        if (slotIndex >= 0 && slotIndex < 54) {
+            ItemStack stack = this.getSlot(slotIndex).getStack();
+
+            if (stack.getItem() == Items.WRITTEN_BOOK && player instanceof ServerPlayerEntity serverPlayer) {
+                ItemStack original = player.getActiveItem();
+
+                int selectSlot = player.getInventory().getSelectedSlot() + 36;
+                serverPlayer.networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(
+                        0, this.nextRevision(), selectSlot, stack
+                ));
+                serverPlayer.networkHandler.sendPacket(new OpenWrittenBookS2CPacket(Hand.MAIN_HAND));
+                serverPlayer.networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(
+                        0, this.nextRevision(), selectSlot, original
+                ));
+            }
+
             return;
         }
+        if (actionType.equals(SlotActionType.PICKUP_ALL)) return;
 
         super.onSlotClick(slotIndex, button, actionType, player);
     }
