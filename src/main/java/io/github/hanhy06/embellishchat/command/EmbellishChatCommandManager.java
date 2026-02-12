84eb277a5850f455a58f3c86f03e7b694ec772ba
조환희 <whghksgmls@gmail.com>
2026-02-13 00:15:25 +0900

    feat: implement data pack reload functionality in EmbellishChatCommandManager

--- src/main/java/io/github/hanhy06/embellishchat/command/EmbellishChatCommandManager.java
+++ src/main/java/io/github/hanhy06/embellishchat/command/EmbellishChatCommandManager.java
@@ -3,6 +3,10 @@ package io.github.hanhy06.embellishchat.command;
 import io.github.hanhy06.embellishchat.EmbellishChat;
 import io.github.hanhy06.embellishchat.config.Config;
 import io.github.hanhy06.embellishchat.config.ConfigListener;
+import net.minecraft.server.command.ReloadCommand;
+import net.minecraft.server.command.ServerCommandSource;
+
+import java.util.List;
 
 public class EmbellishChatCommandManager implements ConfigListener {
     @Override
@@ -12,6 +16,9 @@ public class EmbellishChatCommandManager implements ConfigListener {
 
         AdminCommand.registerCommand(alias);
         UserCommand.registerCommand(alias);
+
+        ServerCommandSource source = EmbellishChat.SERVER.getCommandSource();
+        ReloadCommand.tryReloadDataPacks(List.of(),source);
     }
 
     public static void registerCommand(){
