6611dedcc0dbce91285d40b11dc7209f29424319
조환희 <whghksgmls@gmail.com>
2026-02-12 17:53:30 +0900

    refactor: streamline command registration logic in EmbellishChatCommandManager

--- src/main/java/io/github/hanhy06/embellishchat/command/EmbellishChatCommandManager.java
+++ src/main/java/io/github/hanhy06/embellishchat/command/EmbellishChatCommandManager.java
@@ -7,15 +7,15 @@ public class EmbellishChatCommandManager implements ConfigListener {
     @Override
     public void onConfigReload(Config newConfig) {
         String alias = newConfig.commandAlias();
-        if (alias == null || alias.isBlank()) alias = Config.createDefault().commandAlias();
+        if (alias == null || alias.isBlank()) return;
 
         AdminCommand.registerCommand(alias);
         UserCommand.registerCommand(alias);
     }
 
     public static void registerCommand(){
-        String alias = Config.createDefault().commandAlias();
-        AdminCommand.registerCommand(alias);
-        UserCommand.registerCommand(alias);
+        String defaultCommand = Config.createDefault().commandAlias();
+        AdminCommand.registerCommand(defaultCommand);
+        UserCommand.registerCommand(defaultCommand);
     }
 }
