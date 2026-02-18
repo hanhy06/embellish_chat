e38eddd77301c20d70e3c011eb7d56ca4781c5a4
조환희 <whghksgmls@gmail.com>
2026-02-18 16:27:28 +0900

    feat: remove telemetry configuration and listener from initialization

--- src/main/java/io/github/hanhy06/embellishchat/EmbellishChat.java
+++ src/main/java/io/github/hanhy06/embellishchat/EmbellishChat.java
@@ -9,7 +9,6 @@ import io.github.hanhy06.embellishchat.styling.StylingProcessor;
 import io.github.hanhy06.embellishchat.styling.util.BubbleUtil;
 import io.github.hanhy06.embellishchat.util.PermissionUtil;
 import io.github.hanhy06.embellishchat.util.PlaceHolderUtil;
-import io.github.hanhy06.embellishchat.util.TelemetryUtil;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
